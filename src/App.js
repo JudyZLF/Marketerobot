650b26e5f504f884f0262557ffb90899a92d2fbf
Unknown <judy_zlf@126.com>
2019-09-29 18:06:06 +1000

    set up the project
    including bootstrap, reactJS

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+//import logo from './logo.svg';
+import './App.css';
+import './Mobile.css';
+
+function App() {
+  return (
+       
+    <div className="container">
+      
+    
+    </div>
+    
+  );
+}
+
+export default App;
